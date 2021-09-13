28190b26cdee40f60a483f2f57931c4611358031
Dusan Tisma <dusan@pop-os.localdomain>
2021-09-13 03:31:05 +0200

    Instructions except draw and key press

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <cstdint>
 #include <fstream>
+#include <ctime>
+#include <SFML/Graphics.hpp>
 
 
 class Cpu 
@@ -9,22 +11,36 @@ public:
     void cycle();
     void execute(uint16_t opcode);
     void load(char* filename);
+    void loop();
+    void print_state();
 private:
     uint8_t Vx[16];
     uint16_t I;
     uint8_t delay;
     uint8_t sound;
     uint16_t pc = 0x200;
-    uint8_t sp;
+    uint8_t sp = 0;
     uint16_t stack[16];
     uint8_t ram[4096];
+    uint32_t display[64 * 32];
     const uint32_t start_adress = 0x200;
 
 };
 
+void test()
+{
+    Cpu cpu;
+    cpu.load("space.ch8");
+    //cpu.print_state();
+    // cpu.execute(0xA250);
+    // cpu.execute(0x6495);
+    // cpu.execute(0x6733);
+    // cpu.execute(0xFF65);
+}
+
 Cpu::Cpu()
 {
-
+    std::srand((unsigned)time(0));
 }
 
 void Cpu::cycle()
@@ -35,7 +51,287 @@ void Cpu::cycle()
 
 void Cpu::execute(uint16_t opcode)
 {
-    
+    uint8_t first = (opcode & 0xF000) >> 3*4;
+    uint8_t x = (opcode & 0x0F00) >> 2*4;
+    uint8_t y = (opcode & 0x00F0) >> 1*4;
+    uint8_t last = opcode & 0x000F;
+    uint16_t nnn = opcode & 0x0FFF;
+    uint16_t kk = opcode & 0x00FF;
+
+    //0 start instructions
+    if(first == 0)
+    {
+        // 00E0 - CLS
+        // Clear the display.
+        if(opcode == 0x00E0)
+        {
+            
+        }
+        // 00EE - RET
+        // Return from a subroutine.
+        if(opcode == 0x00EE)
+        {
+            pc = stack[sp];
+            sp--;
+        }
+    }
+
+    // 1nnn - JP addr
+    // Jump to location nnn.
+    if(first == 1)
+    {
+        pc = nnn;
+    }
+
+    // 2nnn - CALL addr
+    // Call subroutine at nnn.
+    if(first == 2)
+    {
+        sp++;
+        stack[sp] = pc;
+        pc = nnn;
+    }
+
+    // 3xkk - SE Vx, byte
+    // Skip next instruction if Vx = kk.  
+    if(first == 3)
+    {
+        if(Vx[x] == kk) pc += 2;
+    }
+
+    // 4xkk - SNE Vx, byte
+    // Skip next instruction if Vx != kk.
+    if(first == 4)
+    {
+        if(Vx[x] != kk) pc += 2;
+    }
+
+    // 5xy0 - SE Vx, Vy
+    // Skip next instruction if Vx = Vy.
+    if(first == 5 && last == 0)
+    {
+        if(Vx[x] == Vx[y]) pc += 2;
+    }
+
+    // 6xkk - LD Vx, byte
+    // Set Vx = kk.
+    if(first == 6)
+    {
+        Vx[x] = kk;
+    }
+
+    // 7xkk - ADD Vx, byte
+    // Set Vx = Vx + kk.
+    if(first == 7)
+    {
+        Vx[x] = Vx[x] + kk;
+    }
+
+    //8 start instructions
+    if(first == 8)
+    {
+        // 8xy0 - LD Vx, Vy
+        // Set Vx = Vy.
+        if(last == 0)
+        {
+            Vx[x] = Vx[y];
+        }
+        
+        // 8xy1 - OR Vx, Vy
+        // Set Vx = Vx OR Vy.
+        if(last == 1)
+        {
+            Vx[x] = Vx[x] | Vx[y];
+        }
+
+        // 8xy2 - AND Vx, Vy
+        // Set Vx = Vx AND Vy.
+        if(last == 2)
+        {
+            Vx[x] = Vx[x] & Vx[y];
+        }
+
+        // 8xy3 - XOR Vx, Vy
+        // Set Vx = Vx XOR Vy.
+        if(last == 3)
+        {
+            Vx[x] = Vx[x] ^ Vx[y];
+        }
+
+        // 8xy4 - ADD Vx, Vy
+        // Set Vx = Vx + Vy, set VF = carry.
+        if(last == 4)
+        {
+            uint16_t sum = Vx[x] + Vx[y];
+            if(sum > 0x00FF) Vx[0xF] = 1;
+            sum = sum & 0x00FF;
+            Vx[x] = sum;
+        }
+
+        // 8xy5 - SUB Vx, Vy
+        // Set Vx = Vx - Vy, set VF = NOT borrow.
+        if(last == 5)
+        {
+            if(Vx[x] > Vx[y]) Vx[0xF] = 1;
+            else Vx[0xF] = 0;
+            Vx[x] = Vx[x] - Vx[y];
+        }
+
+        // 8xy6 - SHR Vx {, Vy}
+        // Set Vx = Vx SHR 1.
+        if(last == 6)
+        {
+            if(Vx[x] & 0x01 == 0x01) Vx[0xF] = 1;
+            else Vx[0xF] = 0;
+            Vx[x] >>= 1;
+        }
+        // 8xy7 - SUBN Vx, Vy
+        // Set Vx = Vy - Vx, set VF = NOT borrow.
+        if(last == 7)
+        {
+            if(Vx[y] > Vx[x]) Vx[0xF] = 1;
+            else Vx[0xF] = 0;
+            Vx[x] = Vx[y] - Vx[x];
+        }
+
+        // 8xyE - SHL Vx {, Vy}
+        // Set Vx = Vx SHL 1.
+        if(last == 0xE)
+        {
+            if((Vx[x] & 0x80) == 0x80) Vx[0xF] = 1;
+            else Vx[0xF] = 0;
+            Vx[x] <<= 1;
+        }
+    }
+
+    // 9xy0 - SNE Vx, Vy
+    // Skip next instruction if Vx != Vy.
+    if(first == 9 && last == 0)
+    {
+        if(Vx[x] != Vx[y])
+        {
+            pc += 2;
+        }
+    }
+
+    // Annn - LD I, addr
+    // Set I = nnn.
+    if(first == 0xA)
+    {
+        I = nnn;
+    }
+
+    // Bnnn - JP V0, addr
+    // Jump to location nnn + V0.
+    if(first == 0xB)
+    {
+        pc = nnn + Vx[0];
+    }
+
+    // Cxkk - RND Vx, byte
+    // Set Vx = random byte AND kk.
+    if(first == 0xC)
+    {
+        Vx[x] = (rand() % 0xFF) & kk;
+    }
+
+    // Dxyn - DRW Vx, Vy, nibble
+    // Display n-byte sprite starting at memory location I 
+    // at (Vx, Vy), set VF = collision
+    if(first == 0xD)
+    {
+        printf("Draw\n");
+    }
+
+    // Ex9E - SKP Vx
+    // Skip next instruction if key with the value of Vx is pressed.
+    if(first == 0xE && kk == 0x9E)
+    {
+
+    }
+
+    // ExA1 - SKNP Vx
+    // Skip next instruction if key with the value of Vx is not pressed.
+    if(first == 0xE && kk == 0xA1)
+    {
+
+    }
+
+    //F start instructions
+    if(first == 0xF)
+    {
+        // Fx07 - LD Vx, DT
+        // Set Vx = delay timer value.
+        if(kk == 0x07)
+        {
+            Vx[x] = delay;
+        }
+
+        // Fx0A - LD Vx, K
+        // Wait for a key press, store the value of the key in Vx.
+        if(kk == 0x0A)
+        {
+
+        }
+
+        // Fx15 - LD DT, Vx
+        // Set delay timer = Vx.
+        if(kk == 0x15)
+        {
+            delay = Vx[x];
+        }
+
+        // Fx18 - LD ST, Vx
+        // Set sound timer = Vx.
+        if(kk == 0x18)
+        {
+            sound = Vx[x];
+        }
+
+        // Fx1E - ADD I, Vx
+        // Set I = I + Vx.
+        if(kk == 0x1E)
+        {
+            I = I + Vx[x];
+        }
+
+        // Fx29 - LD F, Vx
+        // Set I = location of sprite for digit Vx.
+        if(kk == 0x29)
+        {
+
+        }
+
+        // Fx33 - LD B, Vx
+        // Store BCD representation of Vx in memory locations I, I+1, and I+2.
+        if(kk == 0x33)
+        {
+            ram[I] = Vx[x] / 100;
+            ram[I+1] = (Vx[x] % 100) / 10;
+            ram[I+2] = Vx[x] % 10;
+        }
+
+        // Fx55 - LD [I], Vx
+        // Store registers V0 through Vx in memory starting at location I.
+        if(kk == 0x55)
+        {
+            for(int i=0;i<=x;i++)
+            {
+                ram[I+i] = Vx[i];
+            }
+        }
+
+        // Fx65 - LD Vx, [I]
+        // Read registers V0 through Vx from memory starting at location I.
+        if(kk == 0x65)
+        {
+            for(int i=0;i<=x;i++)
+            {
+                Vx[i] = ram[I+i];
+            }
+        }
+    }
+    print_state();
 }
 
 void Cpu::load(char* filename)
@@ -56,11 +352,76 @@ void Cpu::load(char* filename)
     }
 }
 
+void Cpu::loop()
+{
+    // sf::RenderWindow window(sf::VideoMode(64, 32), "CHIP8");
+    // window.setVerticalSyncEnabled(true);
+    // window.setFramerateLimit(60);
+
+    // sf::View view(sf::Vector2f(32.f, 16.f),sf::Vector2f(64.f, 32.f));
+    // window.setView(view);
+
+    // float deltaTime;
+    // float fps;
+    // sf::Clock clock;
+    // std::srand((unsigned)time(0));
+
+    // while (window.isOpen())
+    // {
+    //     //deltaTime / fps
+    //     deltaTime = clock.restart().asSeconds();
+    //     fps = 1.f / deltaTime;
+
+    //     //events
+    //     sf::Event event; 
+    //     while (window.pollEvent(event))
+    //     {
+    //         switch(event.type)
+    //         {
+    //             case sf::Event::Closed:
+    //                 window.close();
+    //                 break;
+    //             case sf::Event::Resized:
+    //                 view.setCenter(window.getView().getCenter());
+    //                 view.setSize(sf::Vector2f(event.size.width, event.size.height));
+    //                 window.setView(view);
+
+    //                 break;
+    //         }
+    //     }
+    //     window.display();
+    // }
+}
+
+void Cpu::print_state()
+{
+    printf("Program counter: %X\n", pc);
+    printf("I register: %X\n", I);
+    printf("Delay timer: %X\n", delay);
+    printf("Sound timer: %X\n", sound);
+    printf("### Registers ###\n");
+    for(int i=0;i<16;i++)
+    {
+        printf("\tV%d:\t%X\n", i, Vx[i]);
+    }
+    printf("\n### Stack ###\n");
+    printf("Stack pointer: %X\n", sp);
+    for(int i=0;i<16;i++)
+    {
+        printf("%d:\t%X\n", i, stack[i]);
+    }
+    printf("------------------------\n");
+}
+
 int main()
 {
     // Cpu *cpu = new Cpu();
     // Cpu cpu = Cpu();
-    Cpu cpu;
-    cpu.load("space.ch8");
+    
+
+    test();
+
+
+    
     return 0;
 }
---
+++ run.sh
@@ -0,0 +1,3 @@
+g++ -c main.cpp
+g++ main.o -o CHIP8 -lsfml-graphics -lsfml-window -lsfml-system
+./CHIP8
